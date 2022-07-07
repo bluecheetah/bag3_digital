41c372a5cdd9c4e8237b3390aa0eafa1b698b6bd
hyunjaekwon <hyunjaekwon@gmail.com>
2022-07-07 14:47:04 -0700

    added optional buffer between SA and SR latch; added optional dummy buffer for layout purposes

--- src/bag3_digital/schematic/flop_strongarm.py
+++ src/bag3_digital/schematic/flop_strongarm.py
@@ -15,7 +15,7 @@
 
 # -*- coding: utf-8 -*-
 
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 import pkg_resources
 from pathlib import Path
@@ -46,19 +46,45 @@ class bag3_digital__flop_strongarm(Module):
         return dict(
             sa_params='strongarm frontend parameters.',
             sr_params='sr latch parameters.',
+            midbuf_params='Optional buffer parameters for output of the strongarm frontend',
+            dum_buf_params='Optional dummy buffer parameters',
             has_rstlb='True to add rstlb functionality.',
             export_mid='True to export intermediate nodes',
         )
 
     @classmethod
     def get_default_param_values(cls) -> Dict[str, Any]:
-        return dict(has_rstlb=False, export_mid=False)
+        return dict(has_rstlb=False, export_mid=False, midbuf_params=None, dum_buf_params=None)
 
-    def design(self, sa_params: Param, sr_params: Param, has_rstlb: bool, export_mid: bool) -> None:
+    def design(self, sa_params: Param, sr_params: Param, midbuf_params: Optional[Param],
+               dum_buf_params: Optional[Param], has_rstlb: bool, export_mid: bool) -> None:
+        if midbuf_params is None:
+            self.remove_instance('XMBUF<1:0>')
+            self.reconnect_instance('XSR', {'sb': 'midn', 'rb': 'midp'}.items())
+            needs_inbuf = True
+            nets_to_export = []
+        else:
+            self.instances['XMBUF<1:0>'].design(**midbuf_params)
+            midbuf_master: Module = self.instances['XMBUF<1:0>'].master
+            num_stg = len(midbuf_master.params['inv_params'])
+            dual_out = midbuf_master.params['dual_output']
+            needs_inbuf = num_stg == 1 or not dual_out
+            nets_to_export = ['midp_buf', 'midn_buf']
+            if needs_inbuf and num_stg % 2 == 1:
+                raise ValueError("Mid buffer must have an even number of stages if SR latch input buffer is required")
+            if dual_out:
+                self.reconnect_instance('XSR', {'s': 'midn_bufb', 'r': 'midp_bufb'}.items())
+                nets_to_export.extend(['midp_bufb', 'midn_bufb'])
+
+        if dum_buf_params is None:
+            self.remove_instance('XDUM')
+        else:
+            self.instances['XDUM'].design(**dum_buf_params)
+
         inbuf_test = sr_params.get('inbuf_params', None)
-        if inbuf_test is None:
+        if needs_inbuf and inbuf_test is None:
             raise ValueError('SR latch must have input buffers.')
-
+        
         self.instances['XSA'].design(has_rstb=has_rstlb, **sa_params.copy(append=dict(export_mid=export_mid)))
         self.instances['XSR'].design(has_rstb=has_rstlb, **sr_params)
 
@@ -68,6 +94,9 @@ class bag3_digital__flop_strongarm(Module):
         if export_mid:
             self.add_pin('midp', TermType.inout)
             self.add_pin('midn', TermType.inout)
+
+            for net in nets_to_export:
+                self.add_pin(net, TermType.inout)
 
             sa_debug_conns = {pin: f'fe_{pin}' for pin in ['midp', 'midn', 'tail']}
             self.reconnect_instance('XSA', sa_debug_conns.items())
